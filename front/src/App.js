c73b27ff2db58af98ba5f7526bbc310617da2878
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-06-02 11:49:09 +0330

    feat: integration back front

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,19 @@
 function App() {
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://localhost:8000');
+      const data = await response.text();
+      console.log(data);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
   return (
   <body>
     <h1>Welcome to the Microservice App!</h1>
     <p>Frontend service is up and running.</p>
+    <button onClick={handleClick}>Send Request</button>
   </body>
   );
 }
