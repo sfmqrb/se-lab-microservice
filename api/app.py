6dc0ef591566ec2af45c06ff294bc2be82ad277a
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-06-02 11:48:46 +0330

    feat: add cors policy

--- api/app.py
+++ api/app.py
@@ -1,12 +1,14 @@
-# app.py
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def hello():
     return "Backend service is up and running."
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-
